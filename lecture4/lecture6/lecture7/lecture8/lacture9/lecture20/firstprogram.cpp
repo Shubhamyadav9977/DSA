e07261e6b118ae6e7883942e60b46e0c78e216cb
Shubhamyadav9977 <shubhauadav@gmail.com>
2026-02-11 20:36:33 +0530

    boston number

---
+++ lecture4/lecture6/lecture7/lecture8/lacture9/lecture20/firstprogram.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int og=n;
+  int temp=n;
+  int sum=0;
+  int l=0;
+  while(temp!=0){
+  temp=temp/10;
+  l++;
+}
+   while(n!=0)
+   {
+    int k=n%10;
+    sum=sum+pow(k,l);
+    n=n/10;
+   }
+    cout<<l<<" "<<sum<<endl;
+    if(sum==og)
+    {
+        cout<<"Armstrong Number";
+    }
+    else
+    {
+        cout<<"Not an Armstrong Number";
+    }
+
+
+  return 0;
+}
